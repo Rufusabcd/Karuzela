9af39e02449ed5e5ea70e28e5bba64986c45c33c
Marcin Grzeniewski <MarcinGrzeniewski@MacBook-Air-Marcin.local>
2017-10-04 22:13:56 +0200

    changes3

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 $(function(){
     var carouselList = $("#carousel ul");
+        
     setInterval(changeSlide,3000);
-
+    
     function changeSlide(){
-        carouselList.animate({marginLeft:-400}, 500, moveFirstSlide);
-      }
-    function moveFristSlide() {
+      carouselList.animate({marginLeft:-400}, 400, moveFristSlide);
+    }
+
+    function moveFristSlide(){
         var firstItem = carouselList.find("li:first");
         var lastItem = carouselList.find("li:last");
-        lastItem.after(firstItem);
-        carouselList.css({marginLeft:0});
+        $(lastItem.after(firstItem)
+    )
+      $(carouselList.css({marginLeft:0}));
     };
-});
+  });
